4aaa1e3e84ace765dc06338a0e00da73de4392b8
Isidro Lopez <islomar@gmail.com>
2015-01-13 11:16:12 +0100

    First version with one simple QuoteController

---
+++ qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -0,0 +1,19 @@
+package qotd
+
+/**
+ * By convention, all the actions over the URLS /qotd/quote would go to this QuoteController.
+ */
+class QuoteController {
+
+    //index() is the action called when the user omits the action name
+    //def index() { }
+
+    static defaultAction = "home"
+
+    //This is an action
+    def home() {
+        //The render() method sends content directly back to the browser
+        render "<h1>Real Programmers do not eat Quiche</h1>"
+    }
+
+}
