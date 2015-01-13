e958c7a06d0459277c3cced3c6db209f6c0a4c96
Isidro Lopez <islomar@gmail.com>
2015-01-13 13:18:48 +0100

    Adding constraints for new Quotes (author and content)

--- qotd/grails-app/domain/qotd/Quote.groovy
+++ qotd/grails-app/domain/qotd/Quote.groovy
@@ -7,5 +7,9 @@ class Quote {
     Date created = new Date()
 
     static constraints = {
+        author(blank: false)
+        //Fields with constraint sizes greater than 255 characters are rendered as HTML <textarea> elements
+        //rather than <input> fields
+        content(maxSize: 1000, blank: false)
     }
 }
