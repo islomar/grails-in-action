1b7b3ee6629a308b6557c43860603787f4265fc7
Isidro Lopez <islomar@gmail.com>
2015-01-19 13:03:36 +0100

    Created second integration test for update

--- hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
+++ hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
@@ -20,8 +20,27 @@ class UserIntegrationSpec extends Specification {
         joe.save()
 
         then: "it saved successfully and can be found in the database"
+        //Ensures save() was error free
         joe.errors.errorCount == 0
+        //confirms save() set database ID
         joe.id != null
         User.get(joe.id).loginId == joe.loginId
+    }
+
+    def "Updating a saved user changes its properties"() {
+
+        given: "An existing user"
+        def existingUser = new User(loginId: 'joe', password: 'secret',
+                           homepage: 'http://www.grailsinaction.com')
+        //failOnError:true means that Grails will throw an exception if the object fails any validation tests
+        existingUser.save(failOnError: true)
+
+        when: "A property is changed"
+        def foundUser = User.get(existingUser.id)
+        foundUser.password = 'sesame'
+        foundUser.save(failOnError: true)
+
+        then: "The change is reflected in the database"
+        User.get(existingUser.id).password == 'sesame'
     }
 }
