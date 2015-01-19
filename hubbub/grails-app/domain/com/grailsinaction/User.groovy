1f675085289e382b2303ae023717e085bd372a7a
Isidro Lopez <islomar@gmail.com>
2015-01-19 13:26:55 +0100

    Added integration test with validation errors

--- hubbub/grails-app/domain/com/grailsinaction/User.groovy
+++ hubbub/grails-app/domain/com/grailsinaction/User.groovy
@@ -8,6 +8,10 @@ class User {
     //Convention names for date fields: **Created and **Updated
     Date dateCreated
 
+    // parentheses are allowed as well to define the constraints
     static constraints = {
+        loginId size: 3..20, unique: true, nullable: false
+        password size: 6..8, nullable: false
+        homepage url: true, nullable: true
     }
 }
--- hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
+++ hubbub/test/integration/com/grailsinaction/UserIntegrationSpec.groovy
@@ -13,49 +13,68 @@ class UserIntegrationSpec extends Specification {
     def "Saving our first user to the database"() {
 
         given: "A brand new user"
-        def joe = new User(loginId: 'joe', password: 'secret',
-                           homepage: 'http://www.grailsinaction.com')
+            def joe = new User(loginId: 'joe', password: 'secret',
+                               homepage: 'http://www.grailsinaction.com')
 
         when: "the user is saved"
-        joe.save()
+            joe.save()
 
         then: "it saved successfully and can be found in the database"
-        //Ensures save() was error free
-        joe.errors.errorCount == 0
-        //confirms save() set database ID
-        joe.id != null
-        User.get(joe.id).loginId == joe.loginId
+            //Ensures save() was error free
+            joe.errors.errorCount == 0
+            //confirms save() set database ID
+            joe.id != null
+            User.get(joe.id).loginId == joe.loginId
     }
 
     def "Updating a saved user changes its properties"() {
 
         given: "An existing user"
-        def existingUser = new User(loginId: 'joe', password: 'secret',
-                           homepage: 'http://www.grailsinaction.com')
-        //failOnError:true means that Grails will throw an exception if the object fails any validation tests
-        existingUser.save(failOnError: true)
+            def existingUser = new User(loginId: 'joe', password: 'secret',
+                               homepage: 'http://www.grailsinaction.com')
+            //failOnError:true means that Grails will throw an exception if the object fails any validation tests
+            existingUser.save(failOnError: true)
 
         when: "A property is changed"
-        def foundUser = User.get(existingUser.id)
-        foundUser.password = 'sesame'
-        foundUser.save(failOnError: true)
+            def foundUser = User.get(existingUser.id)
+            foundUser.password = 'sesame'
+            foundUser.save(failOnError: true)
 
         then: "The change is reflected in the database"
-        User.get(existingUser.id).password == 'sesame'
+            User.get(existingUser.id).password == 'sesame'
     }
 
     def "Deleting an existing user removes it from the database"() {
 
         given: "An existing user"
-        def user = new User(loginId: 'joe', password: 'secret',
-                                    homepage: 'http://www.grailsinaction.com')
-        user.save(failOnError: true)
+            def user = new User(loginId: 'joe', password: 'secret',
+                                        homepage: 'http://www.grailsinaction.com')
+            user.save(failOnError: true)
 
         when: "A property is deleted"
-        def foundUser = User.get(user.id)
-        foundUser.delete(flush: true)
+            def foundUser = User.get(user.id)
+            foundUser.delete(flush: true)
 
         then: "The user is removed from the database"
-        !User.exists(foundUser.id)
+            !User.exists(foundUser.id)
+    }
+
+    def "Saving a user with invalid properties causes an error"() {
+
+        given: "A user which fails several field validations"
+            def user = new User(loginId: 'joe', password: 'tiny',
+                                homepage: 'not-a-url')
+
+        when: "The user is validated"
+            user.validate()
+
+        then:
+            user.hasErrors()
+            "size.toosmall" == user.errors.getFieldError("password").code
+            "tiny"          == user.errors.getFieldError("password").rejectedValue
+            "url.invalid"   == user.errors.getFieldError("homepage").code
+            "not-a-url"     == user.errors.getFieldError("homepage").rejectedValue
+            !user.errors.getFieldError("loginId")
+
     }
 }
